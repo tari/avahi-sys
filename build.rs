f881b4716faf22359abd2f90e5b69eaa4a5c2df1
Walker Crouse <walker.crouse@coop.co.uk>
2020-08-12 14:03:36 -0400

    Upgrade bindgen
    Signed-off-by: Walker Crouse <walker.crouse@coop.co.uk>

--- build.rs
+++ build.rs
@@ -6,11 +6,11 @@ use std::io::{BufRead, BufReader};
 use std::path::PathBuf;
 
 fn whitelist_from_file<F>(mut builder: bindgen::Builder, name: &str, mut f: F) -> bindgen::Builder
-        where F: FnMut(bindgen::Builder, String) -> bindgen::Builder {
-
+where
+    F: FnMut(bindgen::Builder, String) -> bindgen::Builder,
+{
     let infile: PathBuf = env::var_os("CARGO_MANIFEST_DIR").unwrap().into();
-    let infile = File::open(infile.join(name))
-        .expect("Failed to open whitelist file");
+    let infile = File::open(infile.join(name)).expect("Failed to open whitelist file");
 
     for line in BufReader::new(infile).lines() {
         let mut line: String = line.expect("whitelist IO error");
@@ -32,14 +32,18 @@ fn main() {
     println!("cargo:rustc-link-lib=avahi-common");
 
     let mut bindings = bindgen::Builder::default()
-        .no_unstable_rust()
         .whitelist_recursively(false)
         .ctypes_prefix("::libc")
+        .size_t_is_usize(true)
         .header("wrapper.h")
         .bitfield_enum("AvahiClientFlags");
 
     bindings = whitelist_from_file(bindings, "types.txt", bindgen::Builder::whitelisted_type);
-    bindings = whitelist_from_file(bindings, "functions.txt", bindgen::Builder::whitelisted_function);
+    bindings = whitelist_from_file(
+        bindings,
+        "functions.txt",
+        bindgen::Builder::whitelisted_function,
+    );
 
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings

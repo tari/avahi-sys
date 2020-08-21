6cb4d3066451e15a89e019eb6e8accc574135274
Walker Crouse <Walker.Crouse@bjss.com>
2020-08-21 19:26:17 -0400

    Prevent build errors on other platforms
    Signed-off-by: Walker Crouse <Walker.Crouse@bjss.com>

--- build.rs
+++ build.rs
@@ -28,6 +28,10 @@ where
 }
 
 fn main() {
+    if std::env::var("TARGET").unwrap().contains("-unix") {
+        return;
+    }
+    
     println!("cargo:rustc-link-lib=avahi-client");
     println!("cargo:rustc-link-lib=avahi-common");
 

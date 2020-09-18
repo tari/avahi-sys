0935bb5d2c2028d8365547f33fd1f018998f7a31
Walker Crouse <walker.crouse@coop.co.uk>
2020-09-18 17:56:12 -0400

    Simplify bindgen code
    Signed-off-by: Walker Crouse <walker.crouse@coop.co.uk>

--- build.rs
+++ build.rs
@@ -1,35 +1,9 @@
 extern crate bindgen;
 
 use std::env;
-use std::fs::File;
-use std::io::{BufRead, BufReader};
 use std::path::PathBuf;
 
-fn whitelist_from_file<F>(mut builder: bindgen::Builder, name: &str, mut f: F) -> bindgen::Builder
-where
-    F: FnMut(bindgen::Builder, String) -> bindgen::Builder,
-{
-    let infile: PathBuf = env::var_os("CARGO_MANIFEST_DIR").unwrap().into();
-    let infile = File::open(infile.join(name)).expect("Failed to open whitelist file");
-
-    for line in BufReader::new(infile).lines() {
-        let mut line: String = line.expect("whitelist IO error");
-        if let Some(idx) = line.find('#') {
-            line.truncate(idx);
-        }
-
-        if line.is_empty() {
-            continue;
-        }
-        builder = f(builder, line);
-    }
-
-    builder
-}
-
 fn main() {
-    println!("{}", std::env::var("TARGET").unwrap());
-
     if !std::env::var("TARGET").unwrap().contains("-linux") {
         return;
     }
@@ -37,24 +11,13 @@ fn main() {
     println!("cargo:rustc-link-lib=avahi-client");
     println!("cargo:rustc-link-lib=avahi-common");
 
-    let mut bindings = bindgen::Builder::default()
-        .whitelist_recursively(false)
+    bindgen::Builder::default()
+        .header("wrapper.h")
         .ctypes_prefix("::libc")
         .size_t_is_usize(true)
-        .header("wrapper.h")
-        .bitfield_enum("AvahiClientFlags");
-
-    bindings = whitelist_from_file(bindings, "types.txt", bindgen::Builder::whitelisted_type);
-    bindings = whitelist_from_file(
-        bindings,
-        "functions.txt",
-        bindgen::Builder::whitelisted_function,
-    );
-
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings
+        .bitfield_enum("AvahiClientFlags")
         .generate()
-        .expect("Failed to generate bindings")
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
+        .expect("failed to generate bindings")
+        .write_to_file(PathBuf::from(env::var("OUT_DIR").unwrap()).join("bindings.rs"))
+        .expect("failed to write bindings to file");
 }
--- wrapper.h
+++ wrapper.h
@@ -1,5 +1,14 @@
 #include <avahi-client/client.h>
 #include <avahi-client/lookup.h>
 #include <avahi-client/publish.h>
-#include <avahi-common/simple-watch.h>
+
 #include <avahi-common/address.h>
+#include <avahi-common/alternative.h>
+#include <avahi-common/defs.h>
+#include <avahi-common/domain.h>
+#include <avahi-common/error.h>
+#include <avahi-common/malloc.h>
+#include <avahi-common/simple-watch.h>
+#include <avahi-common/strlst.h>
+#include <avahi-common/thread-watch.h>
+#include <avahi-common/watch.h>
